a586b6139a9bc696de514713d72d15ba11a82e7a
ellamarie005 <ellatolentino05@gmail.com>
2018-11-04 17:12:34 -0800

    finished palindrom problem

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,33 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+   return str.split('').every((char, i) => {
+     // str.length takes the number of characters in the array, 
+     // and i - 1 allows for it to for to the end of the array
+     return char === str[str.length - i -1 ];
+   });
+}
 
 module.exports = palindrome;
+
+// my solution
+// function palindrome(str) {
+//   var pal = str.split('').reverse().join('');
+//   if (str == pal) {return true} else return false;
+// }
+
+// Solution 1
+// function palindrome(str) {
+//   const pal = str.split('').reverse().join('');
+//   return str === pal;
+// }
+
+// Solution 2
+// function palindrome(str) {
+//   return str.split('').every((char, i) => {
+//     // str.length takes the number of characters in the array, 
+//     // and i - 1 allows for it to for to the end of the array
+//     return char === str[str.length - i -1 ];
+//   });
+// }
