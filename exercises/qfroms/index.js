a14121260e0525632e2b95502688d4dff6f05569
ellamarie005 <ellatolentino05@gmail.com>
2018-11-16 11:37:35 -0800

    solved queue problem

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -12,8 +12,46 @@
 //     q.remove(); // returns 1
 //     q.remove(); // returns 2
 
+//qfroms = queue from stack
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.first = new Stack();
+    this.second = new Stack();
+  }
+
+  add(record) {
+    this.first.push(record);
+  }
+  remove() {
+    while (this.first.peek()) {
+      // const record = this.first.pop();
+      // this.second.push(record);
+      // or it can be written like this,
+      this.second.push(this.first.pop());
+    }
+    const record = this.second.pop();
+
+    while (this.second.peek()) {
+      this.first.push(this.second.pop());
+    }
+    return record;
+  }
+
+  peek() {
+   while (this.first.peek()) {
+     this.second.push(this.first.pop());
+   }
+
+   const record = this.second.peek();
+
+   while (this.second.peek()) {
+     this.first.push(this.second.pop());
+   }
+
+   return record;
+  }
+}
 
 module.exports = Queue;
