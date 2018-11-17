e8a664a473fec4e88b877ae8b5fa424c5857e94d
ellamarie005 <ellatolentino05@gmail.com>
2018-11-16 22:08:32 -0800

    finished insertFirst

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -3,12 +3,28 @@
 // See 'directions' document
 
 class Node {
+  // next = null as default so when no next is passed through it will automatically equal to null
   constructor(data, next = null) {
     this.data = data;
     this.next = next;
   }
 }
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    //insertFirst = will create it then you have to link it
+    // const node = newNode(data, this.head);
+    // need to update that this.head is now the node
+    // this.head = node;
+
+    //all that can be written like this.
+    this.head = new Node(data, this.head);
+  }
+}
 
 module.exports = { Node, LinkedList };
+
